48f232910a7592602bc70652863ea82326d6e368
Jordan Dziat <jdziat@gmail.com>
2020-08-23 10:51:20 -0400

    Updated events being emitted, readme, and construction

--- test/index.test.js
+++ test/index.test.js
@@ -18,8 +18,8 @@ after(function (done) {
 })
 
 describe('Feathers-Knex-Modeller', () => {
-  let testOne = fixtures.testOne
-  let testTwo = fixtures.testTwo
+  const testOne = fixtures.testOne
+  const testTwo = fixtures.testTwo
   testOne.db = db
   testTwo.db = db
   const modelOne = new Modeler(testOne)
@@ -29,36 +29,36 @@ describe('Feathers-Knex-Modeller', () => {
       it('Should wait for table and column to exist if it references it.', async function () {
         // await Promise.all([modelOne.init(), modelTwo.init()])
         await Promise.all([modelTwo.init(), modelOne.init()])
-        let tableOne = await modelOne.hasTable('test')
-        let hasTable = tableOne
+        const tableOne = await modelOne.hasTable('test')
+        const hasTable = tableOne
         expect(hasTable).to.equal(true)
       })
       it('Has the table(s) after init', async function () {
-        let hasTableOne = await db.schema.hasTable(fixtures.testOne.name)
-        let hasTableTwo = await db.schema.hasTable(fixtures.testTwo.name)
+        const hasTableOne = await db.schema.hasTable(fixtures.testOne.name)
+        const hasTableTwo = await db.schema.hasTable(fixtures.testTwo.name)
         expect(hasTableOne).to.equal(true)
         expect(hasTableTwo).to.equal(true)
       })
       it('Has the columns(s) after init', async function () {
-        let hasTableOneColumnOne = await db.schema.hasColumn(fixtures.testOne.name, fixtures.testOne.columns[0].name)
-        let hasTableTwoDependentColumn = await db.schema.hasColumn(fixtures.testTwo.name, 'test_id')
+        const hasTableOneColumnOne = await db.schema.hasColumn(fixtures.testOne.name, fixtures.testOne.columns[0].name)
+        const hasTableTwoDependentColumn = await db.schema.hasColumn(fixtures.testTwo.name, 'test_id')
         expect(hasTableOneColumnOne).to.equal(true)
         expect(hasTableTwoDependentColumn).to.equal(true)
       })
       it('Specific type should create a column of the requested type', async function () {
-        let tableName = fixtures.data.specificType.textArray.table
-        let columnName = fixtures.data.specificType.textArray.name
-        let columnData = fixtures.data.specificType.textArray.data
+        const tableName = fixtures.data.specificType.textArray.table
+        const columnName = fixtures.data.specificType.textArray.name
+        const columnData = fixtures.data.specificType.textArray.data
         await db(tableName).delete()
-        let dataToInsert = {
-          'name': 'name',
-          'schema_type': 'specificTypeTest',
-          'status': 'TBD',
-          'shared': true
+        const dataToInsert = {
+          name: 'name',
+          schema_type: 'specificTypeTest',
+          status: 'TBD',
+          shared: true
         }
         dataToInsert[columnName] = columnData
         await db.insert(dataToInsert).into(tableName)
-        let returnedData = (await db.select().from(tableName))[0]
+        const returnedData = (await db.select().from(tableName))[0]
         expect(returnedData[columnName]).to.be.an('array')
         expect(returnedData[columnName][0]).to.be.an('string')
       })
